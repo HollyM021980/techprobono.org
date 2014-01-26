6d80cceca5a3ea6974b7b25342f6273cb6e066f9
Andrei Ursan <hello@andreiursan.com>
2014-01-26 13:45:27 +0100

    created the technologist controller

---
+++ app/controllers/technologists_controller.rb
@@ -0,0 +1,9 @@
+class TechnologistsController < ApplicationController
+  respond_to :json
+
+  def show
+  end
+
+  def create
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get "technologists/:id" => "technologists#show"
+  post "technologists/create"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
