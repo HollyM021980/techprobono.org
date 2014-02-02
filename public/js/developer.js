6b4c23cddb817b5b1195f3c48d93d5056ee03643
VeganBen <me@bengreen.org.uk>
2014-02-02 15:09:31 +0100

    completed password signup template

--- public/js/developer.js
+++ public/js/developer.js
@@ -2,8 +2,15 @@ $(document).ready(function() {
 
     var documentHeight = $(document).height();
 
+    $("#signupwithemail").slideUp();
+
+
     $('window').unbind("click");
 
+    $("#emailsignup").click(function(){
+        $('#emailsignup').slideUp();
+        $("#signupwithemail").slideDown();
+    });
 
     var openModal = function(modal) {
         $('window').click(function(modal){
@@ -17,3 +24,26 @@ $(document).ready(function() {
     });
 
 });
+
+
+	var windowClick = function(e) {
+        var el = e.target;
+        e.preventDefault();
+        if(el != triggerElement && !core.isDescendant($("calendar"), el)) {
+            if(isOpen) {
+                hideCalendar();
+            }
+        }
+    };
+
+	var hideCalendar = function() {
+        core.hide($("calendar"));
+        window.removeEventListener("click", windowClick, false);
+        isOpen = false;
+    };
+
+	var showCalendar = function() {
+	    core.show($("calendar"));
+	    window.addEventListener("click", windowClick, false);
+	    isOpen = true;
+	};
