b7ad1fd29f45144f5357187277ae024978440446
VeganBen <me@bengreen.org.uk>
2014-02-03 10:54:49 +0100

    modal handling

--- public/js/developer.js
+++ public/js/developer.js
@@ -1,29 +1,3 @@
-$(document).ready(function() {
-
-    var documentHeight = $(document).height();
-
-    $("#signupwithemail").slideUp();
-
-
-    $('window').unbind("click");
-
-    $("#emailsignup").click(function(){
-        $('#emailsignup').slideUp();
-        $("#signupwithemail").slideDown();
-    });
-
-    var openModal = function(modal) {
-        $('window').click(function(modal){
-            // do open stuff
-        })
-    };
-
-    $('#external_details .addmore').click(function(e){
-        e.preventDefault();
-        createTechListItem(e);
-    });
-
-});
 
 var isDescendant = function (parent, child) {
     var node = child;
@@ -35,6 +9,63 @@ var isDescendant = function (parent, child) {
     }
     return false;
 };
+
+
+$(document).ready(function() {
+
+    var documentHeight = $(document).height();
+
+
+    // $('window').unbind("click");
+
+    var closeModals = function(){
+        $(".modal").addClass("hidden");
+    };
+
+    var openModal = function(modal) {
+        closeModals();
+        $('#' + modal).removeClass("hidden");
+        window.addEventListener("click", (function windowClickDetector(e) {
+            console.log(e.target);
+            debugger;
+            if(e.target === $('#' + modal).get() || isDescendant($('#' + modal).get(), e.target)) {
+                return
+            } else {
+                if(!modal){
+                    closeModals();
+                    window.removeEventListener("click", windowClickDetector, false);
+                }
+            }
+        }), false)
+    };
+
+    $("#signupwithemail").slideUp();
+
+    $("#emailsignup").click(function(){
+        $('#emailsignup').slideUp();
+        $("#signupwithemail").slideDown();
+    });
+
+    $("#external_details .addmore").click(function() {
+        openModal("addContact");
+    });
+
+    $("#updateIntro").click(function() {
+        openModal("addDescription");
+    });
+
+    $("#updateHeadline").click(function() {
+        openModal("addHeadline");
+    });
+
+    openModal("completeSignup");
+
+    $('#external_details .addmore').click(function(e){
+        e.preventDefault();
+        createTechListItem(e);
+    });
+
+});
 
 	var windowClick = function(e) {
         var el = e.target;
