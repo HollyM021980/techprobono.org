c72181ed55806261b7794a1222399debd59aee1b
Andrei Ursan <hello@andreiursan.com>
2014-03-30 23:57:01 +0200

    customised the user resource on the admin panel

--- app/admin/user.rb
+++ app/admin/user.rb
@@ -1,17 +1,36 @@
 ActiveAdmin.register User do
 
-  
-  # See permitted parameters documentation:
-  # https://github.com/gregbell/active_admin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # permit_params :list, :of, :attributes, :on, :model
-  #
-  # or
-  #
-  # permit_params do
-  #  permitted = [:permitted, :attributes]
-  #  permitted << :other if resource.something?
-  #  permitted
-  # end
-  
-end
+  permit_params :email, :password, :password_confirmation,
+                :professional_headline, :account_type, :admin, :name
+
+  form do |f|
+    f.inputs do
+      f.input :email
+      f.input :name
+      f.input :professional_headline
+      f.input :password
+      f.input :password_confirmation
+      f.input :admin
+    end
+    f.actions
+  end
+
+  index do
+    column :email
+    column :name
+    column :professional_headline
+    column :admin
+    default_actions
+  end
+
+
+  show do |ad|
+    attributes_table do
+      row :email
+      row :name
+      row :professional_headlin
+      row :admin
+    end
+    active_admin_comments
+  end
+ end
